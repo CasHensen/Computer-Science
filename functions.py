901ee0a9246d92f05055c44cd3144ce2a885f467
fleurvanexel <117817326+fleurvanexel@users.noreply.github.com>
2023-12-07 00:44:39 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,6 +1,22 @@
 import re
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
+
+
+# Data cleaning of a string
+def dataCleaningOfSingleString(string):
+    string = string.lower()
+    string = string.replace("inches", "inch")
+    string = string.replace("hertz", "hz")
+    string = re.sub(r"(\d+)( *)\"", r'\g<1>inch', string)
+    string = re.sub('[(]?[)]?', "", string)
+    string = re.sub(r':\s', ' ', string)
+    string = re.sub(r'-inch', 'inch', string)
+    string = re.sub(r' - ', ' ', string)
+    string = re.sub(r'(?!<\d)[.](?!\d)', '', string)
+    string = re.sub(' +', ' ', string)
+    return string
+
 
 # Calculate the q-gram similarity for strings q and r
 def calcSim(q, r):
@@ -11,13 +27,13 @@ def calcSim(q, r):
         q_gram_of_q = q[i:i + length_q_gram]
         if q_gram_of_q not in all_q_grams_of_q:
             all_q_grams_of_q.append(q_gram_of_q)
+    print(all_q_grams_of_q)
     intersection = []
-    union = all_q_grams_of_q
+    union = all_q_grams_of_q.copy()
     for i in range(len(r) - length_q_gram + 1):
         q_gram_of_r = r[i:i + length_q_gram]
         if q_gram_of_r not in all_q_grams_of_r:
             all_q_grams_of_r.append(q_gram_of_r)
-            # print("appended to:", all_q_grams_of_r)
             if q_gram_of_r in all_q_grams_of_q:
                 intersection.append(q_gram_of_r)
             if q_gram_of_r not in union:
@@ -35,7 +51,7 @@ def sameShop(p_i, p_j):
 
 # True if brands of products i and j are different
 def diffBrand(p_i, p_j):
-    brands = ["philips", "samsung"]
+    brands = ["akai", "alba", "apple", "arcam", "arise", "bang", "bpl", "bush", "cge", "changhong", "compal", "curtis", "durabrand", "element", "finlux", "fujitsu", "funai", "google", "haier", "hisense", "hitachi", "itel", "jensen", "jvc", "kogan", "konka", "lg", "loewe", "magnavox", "marantz", "memorex", "micromax", "metz", "onida", "panasonic", "pensonic", "philips", "planar", "proscan", "rediffusion", "saba", "salora", "samsung", "sansui", "sanyo", "seiki", "sharp", "skyworth", "sony", "tatung", "tcl", "telefunken", "thomson", "tpv", "tp vision", "vestel", "videocon", "vizio", "vu", "walton", "westinghouse", "xiaomi", "zenith"]
     if any(word in p_i["title"] for word in brands) & any(word in p_j["title"] for word in brands):
         for brand in brands:
             if p_i["title"].__contains__(brand) and p_j["title"].__contains__(brand):
@@ -43,10 +59,30 @@ def diffBrand(p_i, p_j):
             if p_i["title"].__contains__(brand) and not p_j["title"].__contains__(brand) or not p_i["title"].__contains__(brand) and p_j["title"].__contains__(brand):
                 return True
         return True
-    return False # only different brands important
+    return False  # only different brands important
 
-# Find the model words of an input string
-def find_modelWords(string):
+
+# Find the model words of a product (both in title and values)
+def find_modelWordsOfaProduct(product):
+    model_words = []
+    for k in product.keys():
+        if k == "featuresMap":
+            for v in product[k].values():
+                model_words_KVP = model_words + find_modelWordsOfaKVP(v)
+                for word in model_words_KVP:
+                    if word not in model_words:
+                        model_words.append(word)
+        if k != "url" and k != "featuresMap":
+            model_words_string = model_words + find_modelWordsOfaString(product[k])
+            for word in model_words_string:
+                if word not in model_words:
+                    model_words.append(word)
+    return model_words
+
+
+# Find the model words of an input string (helper method)
+def find_modelWordsOfaString(string):
+    string = dataCleaningOfSingleString(string)
     string_splitted = string.split()
     model_words = []
     for w in range(0, len(string_splitted)):
@@ -54,18 +90,17 @@ def find_modelWords(string):
             model_words.append(string_splitted[w])
     return model_words
 
-# The key from key-value pair (KVP) q; value(q) returns the value from KVP q
-# TODO: def key(p):
 
-# All model words from the values of the attributes from product p
-def exMW(p):
+# Find the model words of a KVP (helper method)
+def find_modelWordsOfaKVP(string):
+    string = dataCleaningOfSingleString(string)
+    string_splitted = string.split()
     model_words = []
-    for v in p["featuresMap"].values():
-        words_splitted = v.split()
-        for w in range(0, len(words_splitted)):
-            if re.search(r"(\d+)[^0-9]+|[^0-9]+(\d+)", words_splitted[w]):
-                model_words.append(words_splitted[w])
+    for w in range(0, len(string_splitted)):
+        if re.search(r"(\d+)[^0-9]+|[^0-9]+(\d+)", string_splitted[w]):
+            model_words.append(string_splitted[w])
     return model_words
+
 
 # Percentage of matching model words from two sets of model words
 def mw(C, D):
@@ -73,7 +108,7 @@ def mw(C, D):
     union = C.union(D)
     return float(len(intersection))/float(len(union))
 
-# The TMWM similarity between the products i and j using the parameters α and β
+# The TMWM similarity between the products i and j using the parameters alpha and bete
 # TODO: def TMWMSim(p_i, p_j , alpha, beta):
 
 # The minimum of the number of product features that product i and j contain
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import functions
-import re
+# import re
 
 # Load all data
 with open("TVs-all-merged.json", "r") as file:
@@ -12,24 +12,39 @@ for v in data.values():
     for duplicate in v:
         adjusted_list.append(duplicate)
 
-# Clean titles by removing all common characters (e.g., commas, slashes, white spaces), uppercase to lowercase and consistent unit representation of inch and hertz
+# Store all model words
+all_model_words = []
+model_words_per_item = []
 for i in range(0, len(adjusted_list)):
-    adjusted_list[i]["title"] = adjusted_list[i]["title"].lower()
-    adjusted_list[i]["title"] = adjusted_list[i]["title"].replace("inches", "inch")
-    adjusted_list[i]["title"] = adjusted_list[i]["title"].replace("hertz", "hz")
-    adjusted_list[i]["title"] = re.sub(r"(\d+)( *)\"", r'\g<1>inch', adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub('[(]?[)]?', "", adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub(r':\s', ' ', adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub(r'-inch', 'inch', adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub(r' - ', ' ', adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub(r'(?!<\d)[.](?!\d)', '', adjusted_list[i]["title"])
-    adjusted_list[i]["title"] = re.sub(' +', ' ', adjusted_list[i]["title"])
+    temp_mw = functions.find_modelWordsOfaProduct(adjusted_list[i])
+    for word in temp_mw:
+        if word not in all_model_words:
+            all_model_words.append(word)
+    model_words_per_item.append(temp_mw)
 
-inf_distances = []  # store indexes of columns that respective row has infinite distance with (either different brand or same  shop)
+binary_representation = []
+# Create binary representation
 for i in range(0, len(adjusted_list)):
-    temp_for_inf_distance = []
-    for j in range(0, len(adjusted_list)):
-        if i != j and functions.sameShop(adjusted_list[i], adjusted_list[j]) or functions.diffBrand(adjusted_list[i], adjusted_list[j]):
-            temp_for_inf_distance.append(j)
-    inf_distances.append(temp_for_inf_distance)
-print(inf_distances)
+    model_words_of_item_i = model_words_per_item[i]
+    binary_model_words_of_i = []
+    for element in range(0, len(all_model_words)):
+        if model_words_of_item_i.__contains__(all_model_words[element]):
+            binary_model_words_of_i.append(element)
+    binary_representation.append(binary_model_words_of_i)
+
+# inf_distances = []  # store indexes of columns that respective row has infinite distance with (either different brand or same shop)
+# for i in range(0, len(adjusted_list)):
+#     temp_for_inf_distance = []
+#     for j in range(0, len(adjusted_list)):
+#         if i != j and functions.sameShop(adjusted_list[i], adjusted_list[j]) or functions.diffBrand(adjusted_list[i], adjusted_list[j]):
+#             temp_for_inf_distance.append(j)
+#     inf_distances.append(temp_for_inf_distance)
+# print(inf_distances)
+
+# TODO: use calcSim(q, r) for q-gram similarity (but check function first!!)
+# TODO: use mw(C, D) for percentage of matching model words from two sets of model words
+
+# test_str = "aaaaa"
+# bla = "aaa"
+# print(functions.calcSim(test_str, bla))
+# functions.calcSim(test_str, test_str2) # IS NOT RIGHT
