0bb19c692a0c3709e465a448f19153a97ea1e240
CasHensen <102985001+CasHensen@users.noreply.github.com>
2023-12-15 23:25:40 +0100

    Add files via upload

---
+++ extension_535903.py
@@ -0,0 +1,25 @@
+import re
+import random
+import numpy as np
+from random import randint
+from numpy import unique
+
+def differentResolution(clean_title_pi, clean_title_pj):
+    res_of_pi = unique(re.findall(r"\s(\d+)p\s", clean_title_pi))
+    res_of_pj = unique(re.findall(r"\s(\d+)p\s", clean_title_pj))
+
+    if len(res_of_pi) == 0 or len(res_of_pj) == 0:
+        return False
+
+    if res_of_pi != res_of_pj:
+        return True
+
+    return False
+
+
+
+
+
+
+
+
